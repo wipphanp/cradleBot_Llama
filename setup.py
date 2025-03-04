66170c299931949d4034a7c4472d0f20a0d08481
meetpruthvi <pruthviraj.phanendrakumar@meetpoppins.com>
2025-03-04 19:23:53 +0530

    setup.py added to resolve the error

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Cradle Chatbot',
+    version= '0.0.1',
+    author= 'Pruthviraj S P',
+    author_email= 'wipphanp@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
